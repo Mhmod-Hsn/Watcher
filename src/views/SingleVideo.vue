02621277a4924c89287f9974bdb535b4b616344f
Mahmoud Hassan <hsn.mhmod.96@gmail.com>
2020-09-18 17:33:08 +0200

    add adult skipper

--- src/views/SingleVideo.vue
+++ src/views/SingleVideo.vue
@@ -1,17 +1,17 @@
 <template>
-	<LoadingCard v-if="!item" />
+	<LoadingCard v-if="!item"/>
 	<div
-		id="single-video"
 		class="container py-lg-5 py-md-3 py-2"
+		id="single-video"
 		v-else>
 		
 		<div class="row">
 			<div class="col-md-4 col-sm-6">
 				<b-aspect
-					tag="div"
-					class="position-relative br-10 bg-image poster shadow border"
-					aspect="2:3"
 					:style="{ backgroundImage: `url(${item.poster.formats.medium.url})` }"
+					aspect="2:3"
+					class="position-relative br-10 bg-image poster shadow border"
+					tag="div"
 				>
 				</b-aspect>
 			</div>
@@ -25,25 +25,25 @@
 									{{item.rate}}
 								</b-badge>
 								
-								<b-badge pill variant="primary" class="ml-2">
+								<b-badge class="ml-2" pill variant="primary">
 									{{item.release_year}}
 								</b-badge>
 								
-								<b-badge pill variant="secondary" class="ml-2">
+								<b-badge class="ml-2" pill variant="secondary">
 									{{item.category.name}}
 								</b-badge>
 								
-								<b-badge pill variant="success" class="ml-2">
+								<b-badge class="ml-2" pill variant="success">
 									<i class="far fa-eye"></i>
 									{{item.watch_count}}
 								</b-badge>
 							</div>
 							
-							<b-badge pill variant="danger" v-if="item.adult">
+							<b-badge pill v-if="item.adult" variant="danger">
 								<i class="far fa-exclamation-triangle"></i>
 								+18
 							</b-badge>
-								
+						
 						</div>
 						
 						<h2 class="name">{{item.name}}</h2>
@@ -52,7 +52,7 @@
 							<div class="col-6">
 								Tags:
 								<div class="tags">
-									<b-badge v-for="tag in item.tags" pill variant="secondary" class="mb-1 mr-2">
+									<b-badge class="mb-1 mr-2" pill v-for="tag in item.tags" variant="secondary">
 										{{tag.name}}
 									</b-badge>
 								</div>
@@ -60,7 +60,7 @@
 							<div class="col-6">
 								Actors:
 								<div class="tags">
-									<b-badge v-for="actor in item.actors" pill variant="secondary" class="mb-1 mr-2">
+									<b-badge class="mb-1 mr-2" pill v-for="actor in item.actors" variant="secondary">
 										{{actor.name}}
 									</b-badge>
 								</div>
@@ -70,10 +70,11 @@
 						<p class="story">{{item.story}}</p>
 						
 						<b-button
-							variant="danger"
-							block
 							@click="watchTrailer"
-						>Trailer</b-button>
+							block
+							variant="danger"
+						>Trailer
+						</b-button>
 					
 					</div>
 				</div>
@@ -81,20 +82,20 @@
 		</div>
 		
 		<div class="watch mt-4 shadow">
-			<plyr >
-				<video crossorigin="anonymous">
-					<source :src="'http://localhost:1337'+item.media.url" :type="item.media.mime" />
+			<plyr>
+				<video @timeupdate="watchTime" crossorigin="anonymous" id="player">
+					<source :src="base_url+item.media.url" :type="item.media.mime"/>
 					
 					
 					<!-- Captions are optional -->
 					<track
-						v-for="(sub,index) in item.subtitles"
-						:key="sub.id"
 						:default="index === 0"
-						kind="captions"
+						:key="sub.id"
 						:label="sub.language"
+						:src="base_url+sub.file.url"
 						:srclang="sub.language_code"
-						:src="'http://localhost:1337'+sub.file.url"
+						kind="captions"
+						v-for="(sub,index) in item.subtitles"
 					/>
 				
 				</video>
@@ -105,43 +106,95 @@
 </template>
 
 <script>
-	import Plyr from 'vue-plyr/dist/vue-plyr'
+    import Plyr from 'vue-plyr/dist/vue-plyr'
     import LoadingCard from "../components/LoadingCard";
-	
+
     export default {
         name: "SingleVideo",
-        components: {Plyr,LoadingCard},
+        components: {Plyr, LoadingCard},
         data() {
             return {
                 item: null,
                 subtitleReady: null,
-	            subtitle: null
+                subtitle: null,
+                skipContent: null
             }
         },
         mounted() {
-			// console.log(this.$route.params.id)
+            // console.log(this.$route.params.id)
             this.getVideo();
-            
+
+
+        },
+        computed: {
+            base_url() {
+                return process.env.VUE_APP_BASE_URL
+            },
         },
         methods: {
-            getVideo(){
-                this.$store
-                    .dispatch("video/getVideo", this.$route.params.id)
-                    .then((response) => (this.item = response.data));
+            getVideo() {
+                this.$store.dispatch("video/getVideo", this.$route.params.id)
+                    .then(response => {
+                        this.item = response.data;
+                        this.skipContent = this.parseTimes(response.data.adult_skipper?.content)
+                    })
+            },
+            watchTrailer() {
+                this.$store.commit('video/trailer', this.item.trailer)
+                this.$bvModal.show('trailerModal')
+            },
+            watchTime() {
+                let player = document.getElementById('player');
+
+                /* if (this.skipContent[0].start < player.currentTime && player.currentTime < this.skipContent[0].end){
+					 player.currentTime = this.skipContent[0].end;
+				  
+					 console.log(`skipped from ${this.skipContent[0].start} to ${this.skipContent[0].end}`)
+					 this.skipContent.splice(0,1);
+					 console.log(this.skipContent)
+				  }*/
+
+                if (this.skipContent){
+	                this.skipContent.forEach(scene => {
+	                    if (scene.start < player.currentTime && player.currentTime < scene.end) {
+	                        player.currentTime = scene.end;
+	
+	                        console.log(`skipped from ${scene.start} to ${scene.end}`)
+	                    }
+	                })
+                }
 
             },
-            watchTrailer(){
-                this.$store.commit('video/trailer',this.item.trailer)
-                this.$bvModal.show('trailerModal')
-            }
+            parseTimes(json) {
+                if (json) {
+                    let final = []
+                    json.forEach(item => {
+                        if (isNaN(item.start)) {
+                            let i = item;
 
+                            let temp = {
+                                start: item.start.split(':'),
+                                end: item.end.split(':')
+                            };
+
+                            i.start = (Number(temp.start[0]) * 60 * 60) + (Number(temp.start[1]) * 60) + (Number(temp.start[2]))
+                            i.end = (Number(temp.end[0]) * 60 * 60) + (Number(temp.end[1]) * 60) + (Number(temp.end[2]))
+
+                            final.push(i)
+                        } else {
+                            final.push(item)
+                        }
+                    });
+                    return final
+                }
+            }
         }
     }
 </script>
 
 <style lang="scss" scoped>
-.poster{
-	max-height: 400px;
-}
+	.poster {
+		max-height: 400px;
+	}
 
 </style>
